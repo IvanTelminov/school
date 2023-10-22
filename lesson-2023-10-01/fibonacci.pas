fc7c62076923d6e6c8ad9a5ef057c0f3654fbbd2
IvanTelminov <55989283+IvanTelminov@users.noreply.github.com>
2023-10-22 13:22:24 +0300

    Fibonacci.pas

--- lesson-2023-10-01/fibonacci.pas
+++ lesson-2023-10-01/fibonacci.pas
@@ -1,1 +1,24 @@
+program Fibonacci;
 
+var
+  n, i: integer;
+  F1, F2, F: integer;
+
+begin
+  writeln('Введите количество чисел Фибоначчи: ');
+  readln(n);
+
+  F1 := 0;
+  F2 := 1;
+
+  writeln('Первые ', n, ' чисел Фибоначчи: ');
+
+  for i := 1 to n do
+    begin
+      F := F1 + F2;
+      F1 := F2;
+      F2 := F;
+    end;
+    writeln(F);
+  end;
+end.
